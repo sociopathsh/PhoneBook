e948b1c29d728c6b9303b73cb45ba2968a0abf67
Sherlock <sherlock@gmail.com>
2018-05-02 21:14:08 +0630

    Created Toggle Modal for adding new by using  and prop

--- resources/assets/js/components/Header.vue
+++ resources/assets/js/components/Header.vue
@@ -3,7 +3,8 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
             <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+                <!--<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">-->
+                SH
             </a>
 
             <div class="navbar-menu is-active">
---
+++ resources/assets/js/components/addModal.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <div class="modal" v-bind:class="addModal">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">Modal title</p>
+                <button class="delete" aria-label="close"></button>
+            </header>
+            <section class="modal-card-body">
+                <!-- Content ... -->
+            </section>
+            <footer class="modal-card-foot">
+                <button class="button is-success">Save changes</button>
+                <button class="button" v-on:click="closeAddModal">Cancel</button>
+            </footer>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['addModal'],
+        methods: {
+            closeAddModal: function () {
+                this.$emit('closeAddModal');
+            }
+        }
+    }
+</script>
